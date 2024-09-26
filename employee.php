fe9f51c369bec15f624bcf274081dd2313fb44ec
Mohammed Hammad <mohamedhammad923@gmail.com>
2024-09-26 19:03:13 +0300

    update all

--- employee.php
+++ employee.php
@@ -2,12 +2,26 @@
 
 class employee
 {
-    public $id;
-    public $empName;
-    public $empAge;
-    public $empAddress;
-    public $empTax;
-    public $empSalary;
+    private $id;
+    private $empName;
+    private $empAge;
+    private $empAddress;
+    private $empTax;
+    private $empSalary;
+
+    public function __construct($empName, $empAge, $empAddress, $empTax, $empSalary)
+    {
+        $this->empName = $empName;
+        $this->empAge = $empAge;
+        $this->empAddress = $empAddress;
+        $this->empTax = $empTax;
+        $this->empSalary = $empSalary;
+    }
+
+    public function __get($prop)
+    {
+        return $this->$prop;
+    }
 
     public function calculateSalary()
     {
--- index.php
+++ index.php
@@ -2,46 +2,84 @@
 require_once 'db.php';
 require_once 'employee.php';
 
-// Initialize result variable
 $result = false;
 
-// Check if the form has been submitted
 if (isset($_POST['submit'])) {
-    // Sanitize inputs
     $empName = filter_input(INPUT_POST, 'empName', FILTER_SANITIZE_STRING);
     $empAge = filter_input(INPUT_POST, 'empAge', FILTER_SANITIZE_NUMBER_INT);
     $empAddress = filter_input(INPUT_POST, 'empAddress', FILTER_SANITIZE_STRING);
     $empSalary = filter_input(INPUT_POST, 'empSalary', FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
     $empTax = filter_input(INPUT_POST, 'empTax', FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
 
-    // Create new employee object
-    $employee = new employee($empName, $empAge, $empSalary, $empTax);
-    $employee->empName = $empName;
-    $employee->empAge = $empAge;
-    $employee->empAddress = $empAddress;
-    $employee->empSalary = $empSalary;
-    $employee->empTax = $empTax;
+    $params = [
+        ':empName'   => $empName,
+        ':empAge'    => $empAge,
+        ':empAddress' => $empAddress,
+        ':empSalary' => $empSalary,
+        ':empTax'    => $empTax
+    ];
 
-    // Insert employee into database
-    $sql = 'INSERT INTO employees (empName, empAge, empAddress, empSalary, empTax) 
-            VALUES ("' . $empName . '", ' . $empAge . ', "' . $empAddress . '", ' . $empSalary . ', ' . $empTax . ')';
+    if (isset($_GET['action']) && $_GET['action'] == 'edit' && isset($_GET['id'])) {
+        $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+        $sql = 'UPDATE employees SET empName = :empName, empAge = :empAge, empAddress = :empAddress, empSalary = :empSalary, 
+        empTax = :empTax WHERE id = :id';
+        $params[':id'] = $id;
+    } else {
+        $sql = 'INSERT INTO employees (empName, empAge, empAddress, empSalary, empTax) 
+        VALUES (:empName, :empAge, :empAddress, :empSalary, :empTax)';
+    }
+    $stmt = $connection->prepare($sql);
 
-    // Display success or error message
-    if ($connection->exec($sql)) {
-        // On success, redirect to index.php with a success message
-        header("Location: index.php?add=New record created successfully");
+    if ($stmt->execute($params) === true) {
+        header("Location: index.php?add=Employee saved successfully");
         exit();
     } else {
-        // On error, redirect to index.php with an error message
-        header("Location: index.php?error=Error inserting employee");
+        header("Location: index.php?error=Error saving employee");
         exit();
     }
 }
 
+//UPDATE
+if (isset($_GET['action']) && $_GET['action'] == 'edit' && isset($_GET['id'])) {
+    $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+    if ($id > 0) {
+        $sql = 'SELECT * FROM employees WHERE id = :id';
+        $result = $connection->prepare($sql);
+        $foundUser = $result->execute(array(':id' => $id));
+        if ($foundUser === true) {
+            $user = $result->fetchAll(
+                PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE,
+                'employee',
+                array('empName', 'empAge', 'empAddress', 'empSalary', 'empTax')
+            );
+            $user = array_shift($user);
+        }
+    }
+}
+
+//DELETE
+if (isset($_GET['action']) && $_GET['action'] == 'delete' && isset($_GET['id'])) {
+    $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+    if ($id > 0) {
+        $sql = 'DELETE FROM employees WHERE id = :id';
+        $result = $connection->prepare($sql);
+        $foundUser = $result->execute(array(':id' => $id));
+
+        if ($foundUser) {
+            header("Location: index.php?delete=Employee successfully deleted");
+            exit();
+        } else {
+            header("Location: index.php?error=Failed to delete employee");
+            exit();
+        }
+    }
+}
+
+
 // Retrieve employees from database
 $sql = 'SELECT * FROM employees';
 $stmt = $connection->query($sql);
-$result = $stmt->fetchAll(PDO::FETCH_CLASS, 'employee');
+$result = $stmt->fetchAll(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, 'employee', array('empName', 'empAge', 'empAddress', 'empSalary', 'empTax'));
 $result = (is_array($result) && !empty($result)) ? $result : false;
 
 $pdo = null; // Close the connection
@@ -56,11 +94,12 @@ $pdo = null; // Close the connection
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
         integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <title>Employee Insertion</title>
+    <title>BAT</title>
     <link rel="stylesheet" href="main.css">
 </head>
 
@@ -113,25 +152,30 @@ $pdo = null; // Close the connection
 
                 <div class="form-group">
                     <label for="empName">Employee Name:</label>
-                    <input type="text" id="empName" name="empName" placeholder="Enter employee name">
+                    <input type="text" id="empName" name="empName" placeholder="Enter employee name"
+                        value="<?= isset($user) ? $user->empName : ''; ?>" required>
                 </div>
                 <div class="form-group">
                     <label for="empAge">Employee Age:</label>
-                    <input type="number" id="empAge" name="empAge" placeholder="Enter employee age" min="18" max="60" step="1">
+                    <input type="number" id="empAge" name="empAge" placeholder="Enter employee age" min="18" max="60" step="1"
+                        value="<?= isset($user) ? $user->empAge : ''; ?>" required>
                 </div>
                 <div class="form-group">
                     <label for="empAddress">Employee Address:</label>
-                    <input type="text" id="empAddress" name="empAddress" placeholder="Enter employee address">
+                    <input type="text" id="empAddress" name="empAddress" placeholder="Enter employee address"
+                        value="<?= isset($user) ? $user->empAddress : ''; ?>" required>
                 </div>
                 <div class="form-group">
                     <label for="empSalary">Employee Salary:</label>
-                    <input type="number" id="empSalary" name="empSalary" placeholder="Enter employee salary" min="1500" max="9000" step="1">
+                    <input type="number" id="empSalary" name="empSalary" placeholder="Enter employee salary" min="1500" max="9000" step="1"
+                        value="<?= isset($user) ? $user->empSalary : ''; ?>" required>
                 </div>
                 <div class="form-group">
                     <label for="empTax">Tax Percentage (%):</label>
-                    <input type="number" id="empTax" name="empTax" placeholder="Enter tax percentage" min="1" max="5" step=".01">
+                    <input type="number" id="empTax" name="empTax" placeholder="Enter tax percentage" min="1" max="5" step=".01"
+                        value="<?= isset($user) ? $user->empTax : ''; ?>" required>
                 </div>
-                <div class="form-group">
+                <div class="form-group-submit">
                     <input type="submit" name="submit" value="Submit" class="btn">
                 </div>
             </form>
@@ -140,7 +184,7 @@ $pdo = null; // Close the connection
 
         <!-- Employee List Section -->
         <section class="employees">
-            <h2 id="empList" class="title">Employee List</h2>
+            <h2 id="empList" class="title">Employee Directory</h2>
             <table class="emp-table">
                 <thead>
                     <tr>
@@ -149,6 +193,7 @@ $pdo = null; // Close the connection
                         <th>Address</th>
                         <th>Salary</th>
                         <th>Tax (%)</th>
+                        <th>Actions</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -162,6 +207,15 @@ $pdo = null; // Close the connection
                                 <td><?= $employee->empAddress ?></td>
                                 <td><?= round($employee->calculateSalary()) ?> L.E</td>
                                 <td><?= $employee->empTax ?></td>
+                                <td>
+                                    <a href="../BAT/?action=edit&id=<?= $employee->id ?>" title="Edit">
+                                        <i class="fas fa-edit"></i>
+                                    </a>
+                                    <a href="../BAT/?action=delete&id=<?= $employee->id ?>" title="Delete" onclick="return confirm('Are you sure you want to delete this employee?');">
+                                        <i class="fas fa-trash-alt"></i>
+                                    </a>
+                                </td>
+
                             </tr>
                         <?php
                         }
